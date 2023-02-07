27d34fa9d7228146392c4617a001c5f26ff60bf5
mayiwen <i@mayiwen.com>
2023-02-07 22:54:02 +0800

    #20230207# 深度添加

--- src/app/module/setting/nav-setting/components/test1000/setting-title/test1000.component.ts
+++ src/app/module/setting/nav-setting/components/test1000/setting-title/test1000.component.ts
@@ -1,4 +1,11 @@
-import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { createFalse } from 'typescript/lib/tsserverlibrary';
 import { SelectInterface } from '../../../../../../mayiwen/components/select/o/select';
 // import { NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
@@ -7,89 +14,28 @@ import { SelectInterface } from '../../../../../../mayiwen/components/select/o/s
   selector: 'app-test-1000',
   templateUrl: './test1000.component.html',
   styleUrls: ['./test1000.component.less'],
-  providers: []
+  providers: [],
 })
-export class Test1000Component implements OnInit, AfterViewInit {
-  dataList: SelectInterface[] = [
-    {
-      v: '1111',
-      value: '1111',
-    },
-    {
-      v: '2222',
-      value: '2222',
-    },
-    {
-      v: '3333',
-      value: '3333',
-    },
-    {
-      v: '4444',
-      value: '4444',
-    },
-    {
-      v: '5',
-      value: '5',
-    },
-    {
-      v: '6',
-      value: '6',
-    },
-  ];
-  selectObj: SelectInterface = {
-    v: '5',
-    value: '5',
-  };
-  public tableDate = []; // 表格的数据
-  modalAddFlag = false;
-  modalupdateFlag = false;
-  public alertTitle = '';
-  public alertTitleUpdate = '';
-  public alertTitleid ;
-  private confirmModal;
-  constructor(
-    private cdr: ChangeDetectorRef,
-    ) {
+export class Test1000Component implements AfterViewInit {
+  @ViewChild('canvas') canvas: ElementRef;
+  constructor(private cdr: ChangeDetectorRef) {
 
-  }
-  get selectObjJSON() {
-    return JSON.stringify(this.selectObj);
   }
   ngAfterViewInit(): void {
-    // throw new Error('Method not implemented.');
+    console.log('这是打印的canvas');
+    console.log(this.canvas.nativeElement);
+    const canvas = this.canvas.nativeElement as any;
+    const ctx = canvas.getContext('2d');
+    ctx.moveTo(0,0);
+    ctx.lineTo(400,200);
+    ctx.moveTo(0,200);
+    ctx.lineTo(200,200);
+    ctx.moveTo(0,400);
+    ctx.lineTo(300,300);
+    ctx.moveTo(0,0);
+    ctx.lineTo(400,400);
+    ctx.stroke();
+  }
+  
 
-  }
-  async ngOnInit() {
-    this.getTableData();
-  }
-  getTableData() {
-  }
-  modalAddOk(): void {
-  }
-
-  modalAddCancel(): void {
-    console.log('Button cancel clicked!');
-    this.modalAddFlag = false;
-  }
-  deleteTitle(id) {
-    // this.confirmModal = this.modal.confirm({
-    //   nzTitle: '删除这个条目吗?',
-    //   // nzContent: 'When clicked the OK button, this dialog will be closed after 1 second',
-    //   nzOnOk: () =>
-    //     // new Promise((resolve, reject) => {
-    //     //   setTimeout(Math.random() > 0.5 ? resolve : reject, 1000);
-    //     // }).catch(() => console.log('Oops errors!'))
-    //     this.backendNavService.linkTitleDelete(id).subscribe(res => {
-    //       console.log(res);
-    //       this.getTableData();
-    //     })
-    // });
-  }
-  /** 点击打印的时候的 */
-  change() {
-    this.selectObj = {
-      v: '6',
-      value: '6',
-    };
-  }
 }
